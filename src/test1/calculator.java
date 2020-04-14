6d4f349246bb81e8cd45297c70ab62f241161c08
girma1 <stayonpath1@gmail.com>
2020-04-14 15:35:26 -0600

    add caculator class

---
+++ src/test1/calculator.java
@@ -0,0 +1,17 @@
+package test1;
+
+public class calculator {
+	
+	static int z = 1;
+	static int y = 2;
+	int w = 4;
+	String str;
+	
+	static int sum = z + y + 4;
+	static String result = String.valueOf(sum);
+	
+	
+public static void main (String[] args){
+	System.out.println(result);
+}
+}
--- src/test1/demo_git.java
+++ src/test1/demo_git.java
@@ -4,6 +4,11 @@ public class demo_git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String str;
+		String name = "Git hub now connected";
+		System.out.println(name);
+		
 
 	}
 
